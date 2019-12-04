e7b6beb5bacac913949eed0be5f23abfd04b36c9
Valery Petrov <valeryvpetrov.dev@gmail.com>
2019-12-04 11:40:52 +0600

    Add map marker

--- app/src/main/java/dev/valeryvpetrov/wander/MapsActivity.kt
+++ app/src/main/java/dev/valeryvpetrov/wander/MapsActivity.kt
@@ -37,10 +37,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val mysFligeli = LatLng(81.858250, 59.112159)
+        val zoomLevel = 10f
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mysFligeli, zoomLevel))
+        mMap.addMarker(MarkerOptions().position(mysFligeli).title("Mys Fligeli"))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
